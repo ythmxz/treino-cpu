5bf533839729d94583b90687dce142511ed5b220
Yuri Thomaz <ytcarvalho14@gmail.com>
2026-06-04 23:24:46 -0300

    Resolve problema "enigma" da CPU 2023

---
+++ 2023/enigma/enigma.py
@@ -0,0 +1,43 @@
+from time import perf_counter
+
+
+def main() -> None:
+    zenit: list[str] = ["z", "e", "n", "i", "t", "Z", "E", "N", "I", "T"]
+    polar: list[str] = ["p", "o", "l", "a", "r", "P", "O", "L", "A", "R"]
+
+    entrada: list[str] = []
+    saida: list[str] = []
+
+    index: int = 0
+
+    while True:
+        try:
+            entrada: list[str] = list(input())
+        except EOFError:
+            break
+
+        saida = []
+
+        for caractere in entrada:
+            if caractere in zenit:
+                index = zenit.index(caractere)
+                caractere = polar[index]
+
+            elif caractere in polar:
+                index = polar.index(caractere)
+                caractere = zenit[index]
+
+            saida.append(caractere)
+
+        print("".join(saida))
+
+
+if __name__ == "__main__":
+    inicio: float = perf_counter()
+    main()
+    fim: float = perf_counter()
+
+    tempo: float = fim - inicio
+
+    if tempo > 1.0:
+        print(f"LIMITE DE TEMPO EXCEDIDO ({tempo} s)")
